ff3d2f184d95504a54fe3b8330e5bac330b4b6f3
FrantzCFelix <Frantz.C.Felix@gmail.com>
2022-04-08 11:08:38 -0700

    Added set up display button to hide/show joke setup

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let setUpEL = document.querySelector("#setUp");
 let punchLineEL = document.querySelector("#punchLine");
+let setUpBtnEl = document.querySelector("#setUpBtn")
 
 fetch('https://manateejokesapi.herokuapp.com/manatees/random')
   .then(response => response.json())
   .then(data => {
       console.log(data);
-       //disaply setup clicked
-      setUpEL.textContent = data.setup
+      setUpBtnEl.addEventListener('click',function(){
+        if(setUpEL.getAttribute('data-visible')==='hidden'){
+          setUpEL.setAttribute('data-visible', 'visible');
+          setUpEL.textContent = data.setup
+        }
+        else{
+          setUpEL.setAttribute('data-visible', 'hidden');
+          setUpEL.textContent = ''
+        }
+      })
       punchLineEL.textContent = data.punchline
   });
 
